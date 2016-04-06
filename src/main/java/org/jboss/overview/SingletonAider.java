dbdce096138b196567103e90bb35d35d0bcdfe9d
Chao Wang <chaowan@redhat.com>
2016-04-07 00:33:35 +0800

    comment out problematic @ConcurrencyManagement pull-shared.version 0.5.3

--- src/main/java/org/jboss/overview/SingletonAider.java
+++ src/main/java/org/jboss/overview/SingletonAider.java
@@ -33,8 +33,8 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.AccessTimeout;
-import javax.ejb.ConcurrencyManagement;
-import javax.ejb.ConcurrencyManagementType;
+//import javax.ejb.ConcurrencyManagement;
+//import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
@@ -58,7 +58,7 @@ import org.richfaces.application.push.MessageException;
 
 @Startup
 @Singleton
-@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
+//@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
 public class SingletonAider {
 
     private final Logger LOGGER = Logger.getLogger(SingletonAider.class);
